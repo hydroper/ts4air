b9de1c2287b55c2550677bf392939b1f045bc283
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-17 15:24:46 -0300

    _

--- packages/ts4air/src/abcwriter/abcfile.ts
+++ packages/ts4air/src/abcwriter/abcfile.ts
@@ -26,17 +26,31 @@ export default class AbcFile {
         this.bytes.writeByte(value > -1 ? value >> 16 : -(value >> 16) | 0x80);
     }
 
+    writeU30(value: number) {
+        assert(value >= 0 && value <= 0x3F_FF_FF_FF, `Invalid u30 range, given: ${value}.`);
+        this.writeU32(value);
+    }
+
     writeU32(value: number) {
         assert(value >= 0 && value <= 0xFF_FF_FF_FF, `Invalid u32 range, given: ${value}.`);
         for (;;) {
             let byte = value & 0x7F;
             value >>= 7;
             if (value != 0) {
-                this.writeU8(0b1_0000000 | byte);
+                this.writeU8(0b1000_0000 | byte);
             } else {
                 this.writeU8(byte);
                 break;
             }
         }
+    }
+
+    writeS32(value: number) {
+        assert(value >= 0x80_00_00_00 && value <= 0x7F_FF_FF_FF, `Invalid s32 range, given: ${value}.`);
+        this.writeU32(value >>> 0);
+    }
+
+    writeD64(value: number) {
+        this.bytes.writeDouble(value);
     }
 }
