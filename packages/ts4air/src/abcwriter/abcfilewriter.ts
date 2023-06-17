cb1ae50066aae2b2872bd5287c17bcf1ad424965
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-17 19:25:09 -0300

    _

--- packages/ts4air/src/abcwriter/abcfilewriter.ts
+++ packages/ts4air/src/abcwriter/abcfilewriter.ts
@@ -13,49 +13,53 @@ export default class AbcFileWriter {
         return this.bytes.toNodejsBuffer();
     }
 
-    writeU8(value: number) {
+    u8(value: number) {
         this.bytes.writeByte(value);
     }
 
-    writeU16(value: number) {
+    u16(value: number) {
         this.bytes.writeUnsignedShort(value);
     }
 
-    writeS24(value: number) {
+    s24(value: number) {
         assert(value >= -0x80_00_00 && value <= 0x7F_FF_FF, `Invalid s24 range, given: ${value}.`);
         this.bytes.writeUnsignedShort(-value & 0xFF_FF);
         this.bytes.writeByte(value > -1 ? value >> 16 : -(value >> 16) | 0x80);
     }
 
-    writeU30(value: number) {
+    u30(value: number) {
         assert(value >= 0 && value <= 0x3F_FF_FF_FF, `Invalid u30 range, given: ${value}.`);
-        this.writeU32(value);
+        this.u32(value);
     }
 
-    writeU32(value: number) {
+    u32(value: number) {
         assert(value >= 0 && value <= 0xFF_FF_FF_FF, `Invalid u32 range, given: ${value}.`);
         for (;;) {
             let byte = value & 0x7F;
             value >>= 7;
             if (value != 0) {
-                this.writeU8(0b1000_0000 | byte);
+                this.u8(0b1000_0000 | byte);
             } else {
-                this.writeU8(byte);
+                this.u8(byte);
                 break;
             }
         }
     }
 
-    writeS32(value: number) {
+    s32(value: number) {
         assert(value >= 0x80_00_00_00 && value <= 0x7F_FF_FF_FF, `Invalid s32 range, given: ${value}.`);
-        this.writeU32(value >>> 0);
+        this.u32(value >>> 0);
     }
 
-    writeD64(value: number) {
+    d64(value: number) {
         this.bytes.writeDouble(value);
     }
 
-    writeAbcFile(abcFile: AbcFile) {
+    abcFile(abcFile: AbcFile) {
         unimplementedYet();
+    }
+
+    add() {
+        this.u8(0xA0);
     }
 }
