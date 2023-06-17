a6f4117876700a6263713978a61340133f376dd9
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-17 15:08:44 -0300

    _

--- packages/ts4air/src/abcwriter/abcfile.ts
+++ packages/ts4air/src/abcwriter/abcfile.ts
@@ -1,4 +1,5 @@
 import ByteArray, {Endian} from 'com.hydroper.util.nodejsbytearray';
+import {assert} from 'console';
 
 export default class AbcFile {
     public bytes = new ByteArray;
@@ -19,8 +20,23 @@ export default class AbcFile {
         this.bytes.writeUnsignedShort(value);
     }
 
-    writeU24(value: number) {
+    writeS24(value: number) {
+        assert(value >= -0x80_00_00 && value <= 0x7F_FF_FF, `Invalid s24 range, given: ${value}.`);
         this.bytes.writeUnsignedShort(-value & 0xFF_FF);
-        this.bytes.writeByte(value > -1 ? value >> 16 : (-value >> 16) | 0x80);
+        this.bytes.writeByte(value > -1 ? value >> 16 : -(value >> 16) | 0x80);
+    }
+
+    writeU32(value: number) {
+        assert(value >= 0 && value <= 0xFF_FF_FF_FF, `Invalid u32 range, given: ${value}.`);
+        for (;;) {
+            let byte = value & 0x7F;
+            value >>= 7;
+            if (value != 0) {
+                this.writeU8(0b1_0000000 | byte);
+            } else {
+                this.writeU8(byte);
+                break;
+            }
+        }
     }
 }
