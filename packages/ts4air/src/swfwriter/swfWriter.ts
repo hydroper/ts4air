f27012bdf57e5b13500bc6aedab6f3fe2e1b3366
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-18 10:48:55 -0300

    _

--- packages/ts4air/src/swfwriter/swfWriter.ts
+++ packages/ts4air/src/swfwriter/swfWriter.ts
@@ -1,12 +1,31 @@
 import ByteArray from 'com.hydroper.util.nodejsbytearray';
 import {Rect} from './swf';
 import BitStream from './util/bitstream';
+import {assert} from 'console';
 
 export default class SwfWriter {
-    public bytes: ByteArray = new ByteArray();
+    private bytes: ByteArray = new ByteArray();
+    private lengthOffset: number = 0;
+    private frameCount: number = 0;
+    private frameCountOffset: number = 0;
 
     constructor() {
         this.bytes.endian = 'littleEndian';
+    }
+
+    public toSWFBytes(): ByteArray {
+        this._applyFixups();
+        return this.bytes;
+    }
+
+    private _applyFixups() {
+        this.bytes.position = this.lengthOffset;
+        this.ui32(this.bytes.length);
+
+        this.bytes.position = this.frameCountOffset;
+        this.ui16(this.frameCount);
+
+        this.bytes.position = this.bytes.length;
     }
 
     toNodejsBuffer() {
@@ -47,6 +66,11 @@ export default class SwfWriter {
 
     double(value: number) {
         this.bytes.writeDouble(value);
+    }
+
+    string(str: string) {
+        this.bytes.writeUTF8(str);
+        this.ui8(0);
     }
 
     encodedU32(value: number) {
@@ -91,21 +115,96 @@ export default class SwfWriter {
         this.bytes.writeBytes(bytes);
     }
 
+    fileAttributes(attr: SwfFileAttributes) {
+        let tag = new BitStream();
+        tag.bits(0, 3);
+        tag.bit(attr.hasMetadata ? 1 : 0);
+        tag.bit(attr.actionScript3 ? 1 : 0);
+        tag.bit(attr.suppressCrossDomainCaching ? 1 : 0);
+        tag.bit(0);
+        tag.bit(attr.useNetwork ? 1 : 0);
+        tag.bits(0, 24); // reserved?
+
+        this.tag(69, tag.toBytes());
+    }
+
+    frameLabel(name: string, anchor: boolean = false) {
+        let tag = new SwfWriter();
+        tag.string(name);
+        assert(!anchor);
+        /*
+        if (anchor) {
+            tag.bit(1);
+        }
+        */
+
+        this.tag(43, tag.bytes);
+    }
+
+    showFrame() {
+        this.tag(1, new ByteArray());
+        ++this.frameCount;
+    }
+
+    /**
+     * @param flags Flags represented by `DoABCFlags`.
+     */
+    doABC(name: string, bytecode: ByteArray, flags: number = 0) {
+        let tag = new SwfWriter();
+        tag.ui32(flags);
+        tag.string(name);
+        tag.bytes.writeBytes(bytecode);
+
+        this.tag(82, tag.bytes);
+    }
+
+    symbolClass(symbols: string[], tags: Record<string, number> = {}) {
+        let tag = new SwfWriter();
+        tag.ui16(symbols.length);
+        for (let name of symbols) {
+            let id = tags[name] || 0;
+            tag.ui16(id);
+            tag.string(name);
+        }
+
+        this.tag(76, tag.bytes);
+    }
+
+    end() {
+        this.tag(0, new ByteArray());
+    }
+
     swfHeader(options: SwfHeaderOptions) {
         this.ui8('F'.codePointAt(0));
         this.ui8('W'.codePointAt(0));
         this.ui8('S'.codePointAt(0));
         this.ui8(19);
-        this.ui32(options.fileLength);
+
+        // fileLength
+        this.lengthOffset = this.bytes.position;
+        this.ui32(0);
+
         this.rect(options.frameSize);
         this.ui16(Math.round(options.frameRate * 256));
-        this.ui16(options.frameCount);
+
+        // frameCount
+        this.frameCountOffset = this.bytes.position;
+        this.ui16(0);
     }
 }
 
 export type SwfHeaderOptions = {
-    fileLength: number,
     frameSize: Rect,
     frameRate: number,
-    frameCount: number,
+};
+
+export type SwfFileAttributes = {
+    actionScript3: boolean,
+    useNetwork: boolean,
+    hasMetadata?: boolean,
+    suppressCrossDomainCaching?: boolean,
+};
+
+export const DoABCFlags = {
+    LAZY_INITIALIZE: 1,
 };
